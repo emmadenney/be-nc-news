bfb1160f231efda7bdb41b13457009ec72fbc434
emmadenney <emma_denney@hotmail.com>
2023-02-20 14:05:17 +0000

    Updated first endpoint testing

--- error-handling.js
+++ error-handling.js
@@ -4,3 +4,8 @@ exports.errorHandler500 = (err, request, response, next) => {
   console.log(error);
   response.status(500).send({ msg: "Server error" });
 };
+
+exports.errorHandler400 = (err, request, response, next) => {
+  console.log(error);
+  response.status(400).send({ msg: "Bad request" });
+};
