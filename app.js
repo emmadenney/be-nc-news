010061feab358b4e458cda83654654dee4f1b062
emmadenney <emma_denney@hotmail.com>
2023-02-24 13:42:23 +0000

    Completed task 12

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const {
   getComments,
   getUsers,
   updateVotes,
+  deleteComment,
 } = require("./controller");
 
 app.use(express.json());
@@ -26,6 +27,8 @@ app.get("/api/users", getUsers);
 
 app.post("/api/articles/:article_id/comments", postComment);
 app.patch("/api/articles/:article_id", updateVotes);
+
+app.delete("/api/comments/:comment_id", deleteComment);
 
 app.all("/*", (request, response, next) => {
   response.status(404).send({ msg: "Not found!" });
--- models.js
+++ models.js
@@ -111,3 +111,15 @@ exports.selectUsers = () => {
     return result.rows;
   });
 };
+
+exports.deleteCommentById = (comment_id) => {
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [
+      comment_id,
+    ])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found!" });
+      }
+    });
+};
