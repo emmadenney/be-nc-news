f143e2d6b32cc217acf41ecd729f32f1dcb89af8
emmadenney <emma_denney@hotmail.com>
2023-02-24 14:32:47 +0000

    Updating stuff for hosting

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ require("dotenv").config({
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set");
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 const config =
@@ -14,4 +14,4 @@ const config =
     ? { connectionString: process.env.DATABASE_URL, max: 2 }
     : {};
 
-module.exports = new Pool();
+module.exports = new Pool(config);
