802947dd0216dd1000239535b13086d5425ba5a8
emmadenney <emma_denney@hotmail.com>
2023-05-09 13:27:54 +0100

    added GET username/:username endpoint

--- controller.js
+++ controller.js
@@ -8,6 +8,7 @@ const {
   selectUsers,
   updateArticleVotes,
   deleteCommentById,
+  selectUserByUsername,
 } = require("./models");
 const fs = require("fs/promises");
 
@@ -95,6 +96,17 @@ exports.getUsers = (request, response, next) => {
   selectUsers()
     .then((users) => {
       response.status(200).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUserByUsername = (request, response, next) => {
+  const { username } = request.params;
+  selectUserByUsername(username)
+    .then((user) => {
+      response.status(200).send({ user });
     })
     .catch((err) => {
       next(err);
